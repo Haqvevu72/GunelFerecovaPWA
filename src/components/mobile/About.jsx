d6eff057baafd09807b7ca1a8904cecdd12796df
Haqvevu72 <novruzluqyds20@gmail.com>
2025-01-15 11:27:08 +0400

    description updated

--- src/components/mobile/About.jsx
+++ src/components/mobile/About.jsx
@@ -1,31 +1,37 @@
 import name_az from "@assets/images/name_az.png";
-import desc_az from "@assets/images/desc_az.png";
 
 const About = ({ language }) => {
   const translations = {
     az: {
       name: name_az,
-      description: desc_az
+      description:
+        "Mən endokrin sistem xəstəliklərinin diaqnostikası, müalicəsi və profilaktikası üzrə ixtisaslaşmışam",
     },
     en: {
       name: "Namiq Rzayev",
       description:
-        "CEO & Founder Webonly. Our company specializes in website development, CRM systems, and mobile apps.",
+        "Mən endokrin sistem xəstəliklərinin diaqnostikası, müalicəsi və profilaktikası üzrə ixtisaslaşmışam",
     },
     ru: {
       name: "Намиг Рзаев",
       description:
-        "CEO & Founder Webonly. Наша компания занимается разработкой сайтов, crm и мобильных приложений.",
+        "Mən endokrin sistem xəstəliklərinin diaqnostikası, müalicəsi və profilaktikası üzrə ixtisaslaşmışam",
     },
   };
 
   const content = translations[language] || translations.uz;
 
   return (
-    <div className="mt-24 mr-16">
-      <img id="name" src={content.name} alt=""  className="w-[255.63px] h-[84px]"/>
-      <img id="description" src={content.description} className="w-[283.04px]"/>
-        
+    <div className="h-[84px] mt-24 mr-16">
+      <img
+        id="name"
+        src={content.name}
+        alt=""
+        className="w-[255.63px] h-[84px]"
+      />
+      <p id="description" className="text-white font-bold px-2">
+        {content.description}
+      </p>
     </div>
   );
 };
--- src/components/mobile/Profile.jsx
+++ src/components/mobile/Profile.jsx
@@ -3,8 +3,8 @@ import profileImg from "@assets/images/profile.png";
 const Profile = () => {
   return (
     <div className="w-full min-h-[400px] pt-5">
-      <div className="relative h-[400px] w-[360px] mx-auto">
-        <img className="absolute z-20 left-1/2 transform -translate-x-1/2" src={profileImg} alt="profile" />
+      <div className="relative h-[420px] w-[360px] mx-auto">
+        <img className="absolute z-20 left-1/2 transform -translate-x-1/2 mt-14" src={profileImg} alt="profile" />
       </div>
     </div>
   );
